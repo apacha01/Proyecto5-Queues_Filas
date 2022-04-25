1c047286dbeb764d3ebe5c302628e67823298df6
Agustin Pacheco <agustin.pacheco1901@gmail.com>
2022-04-25 15:01:45 -0300

    Implemento queues (filas) con arrays de tamaño fijo.

--- prueba.c
+++ prueba.c
@@ -8,17 +8,18 @@ int main(){
 	Queue q;
 	initQueue(&q);
 
-	for (int i = 0; i < 20; ++i) {
-		enqueue(&q,i+2);
+	for (int i = 0; i < 10; ++i) {
+		enqueue(&q,i+1);
 	}
 
 	printf("%d\n\n", emptyQueue(&q));
 
-	for (int i = 0; i < 20; ++i) {
+	for (int i = 0; i < 10; ++i) {
 		printf("%d\n", dequeue(&q));
 	}
 
 	printf("\n%d\n", emptyQueue(&q));
+
 
 	return 0;
 }
